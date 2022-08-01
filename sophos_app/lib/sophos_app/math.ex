0d199096ac161fa4bfe9192271def40c42406ac7
crbetan <crbetan@bancolombia.com.co>
2022-08-01 18:39:33 -0500

    Add SophosApp.MCD module - Group Sum and MCD in the same module. Delete mcd.ex

--- sophos_app/lib/sophos_app/math.ex
+++ sophos_app/lib/sophos_app/math.ex
@@ -2,9 +2,11 @@ defmodule SophosApp.Math do
   def sum(a, b) do
     a + b
   end
+  def mcd(a, 0), do: a
+  def mcd(a, b), do: mcd(b, rem(a, b))
 end
 
-defmodule SophosApp.Geometry.Rectangle do
+defmodule Geometry.Rectangle do
   def area(a), do: area(a, a)
   def area(a, b), do: a * b
 end
