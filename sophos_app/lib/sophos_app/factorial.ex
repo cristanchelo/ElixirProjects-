245f985cd80694b50354038a1c171a7e371f285d
crbetan <crbetan@bancolombia.com.co>
2022-08-02 01:54:50 -0500

    Update SophosApp.Fibonacci - Fibonacci with Tail Call optimization

--- sophos_app/lib/sophos_app/factorial.ex
+++ sophos_app/lib/sophos_app/factorial.ex
@@ -1,7 +1,17 @@
 defmodule SophosApp.Factorial do
+  @moduledoc """
+  Cálculo del factorial
+  n! = 1*2*3*4*5...*n
+  """
+  @doc """
+  Factorial - Recursión de pila
+  """
   def of(0), do: 1
   def of(n) when n >= 0, do: n * of(n - 1)
 
+  @doc """
+  Factorial - Recursión de cola
+  """
   def of2(n), do: compute(n, 1)
   defp compute(1, accum), do: accum
   defp compute(n, accum), do: compute(n - 1, accum * n)
--- sophos_app/lib/sophos_app/fibonacci.ex
+++ sophos_app/lib/sophos_app/fibonacci.ex
@@ -1,7 +1,24 @@
 defmodule SophosApp.Fibonacci do
+  @moduledoc """
+  Secuencia de Fibonacci
+  Fib(n) = Fib(n-1) + Fib(n-2) n>=2 n0=0, n1=1
+  """
+  @doc """
+  Fibonacci - Recursión de Pila
+  """
   def sequence(0), do: 0
   def sequence(1), do: 1
   def sequence(n) do
     sequence(n - 1) + sequence(n - 2)
   end
+
+  @doc """
+  Fibonacci - Recursión de Cola
+  """
+  def sequenceTail(n) do
+    sequenceTail(n, %{n1: 0, n2: 1} )
+  end
+  defp sequenceTail(0, %{n1: a, n2: b}), do: a
+  defp sequenceTail(1, %{n1: a, n2: b}), do: b
+  defp sequenceTail(n, map), do: sequenceTail(n - 1, %{n1: map.n2, n2: map.n1 + map.n2} )
 end
