fcae4de246d3ea668ce0244ad19bc1444a65c71a
crbetan <crbetan@bancolombia.com.co>
2022-08-02 18:24:25 -0500

    Update SophosApp.FibonacciServer - Include after reserved word into FibonacciServer.loop function

--- sophos_app/lib/sophos_app/fibonacci_server.ex
+++ sophos_app/lib/sophos_app/fibonacci_server.ex
@@ -1,7 +1,16 @@
 defmodule SophosApp.FibonacciServer do
+  alias SophosApp.Fibonacci
   def loop() do
     receive do
+      {:sequence, n} ->
+        Fibonacci.sequence(n)
+        |> IO.inspect()
+
+      {:status, msg} ->
+        IO.puts("Running #{inspect(msg)}")
       message -> IO.puts(message)
+      after
+        1500 -> IO.puts("Se acabó")
     end
   end
 end
