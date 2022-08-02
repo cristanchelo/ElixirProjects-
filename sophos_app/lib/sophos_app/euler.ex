343cffc8f5f3a8d2966f361c0c03c82a9268744d
crbetan <crbetan@bancolombia.com.co>
2022-08-02 13:19:09 -0500

    Update SophosApp.ProjectEuler - Include filter functions

--- sophos_app/lib/sophos_app/euler.ex
+++ sophos_app/lib/sophos_app/euler.ex
@@ -9,6 +9,14 @@ defmodule SophosApp.ProjectEuler do
     |> MyList.sum()
   end
 
+  def project_1_bizz(n) do
+    n - 1
+    |> MyList.generate()
+    |> filter()
+    |> IO.inspect()
+    |> MyList.sum()
+  end
+
   defp transform(numbers) do
     do_transform(numbers, [])
   end
@@ -21,4 +29,19 @@ defmodule SophosApp.ProjectEuler do
   defp evaluate(h) when rem(h,5) == 0, do: h
   defp evaluate(h) when rem(h,3) == 0, do: h
   defp evaluate(_h), do: 0
+
+  defp filter(numbers) do
+    #filter_fun = fn n -> rem(n,5) == 0 or rem(n,3) == 0 end
+    do_filter(numbers, [])
+  end
+
+  defp do_filter([], filter_list), do: filter_list
+  defp do_filter([h | t], filter_list) do
+    do_filter(t, apply_filter(h, filter_list))
+  end
+  defp apply_filter(h, list)
+      when rem(h,5) == 0 or rem(h,3) == 0,
+      do: [h | list]
+  defp apply_filter(_h, list), do: list
+
 end
