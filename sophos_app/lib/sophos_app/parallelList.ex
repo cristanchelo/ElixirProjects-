4eaec5eae536d4616335711ba6bf36a9a63c179c
crbetan <crbetan@bancolombia.com.co>
2022-08-04 18:04:51 -0500

    UpdateSophosApp.ParallelList

--- sophos_app/lib/sophos_app/parallelList.ex
+++ sophos_app/lib/sophos_app/parallelList.ex
@@ -1,7 +1,8 @@
 defmodule SophosApp.ParallelList do
   def pmap(list, fun) do
     list
-    |> Enum.map(&spawn_process(&1, self(), fun))
+    |> Enum.map(&spawn_process(&1, self(), fun)) #Lista de procesos que ejecutaron algo
+    |> Enum.map(&await/1) #Se recibe cada proceso, se compara o espera el que es con ^ y regresa el resultado
   end
 
   defp spawn_process(e, caller, fun) do
@@ -9,5 +10,11 @@ defmodule SophosApp.ParallelList do
       send(caller, {self(), fun.(e)})
     end
     )
+  end
+
+  defp await(pid) do
+    receive do
+      {^pid, result} -> result
+    end
   end
 end
