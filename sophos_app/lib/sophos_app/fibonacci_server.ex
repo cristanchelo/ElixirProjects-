4e1e8aac95113de39790d543a518e2eb93eaf527
crbetan <crbetan@bancolombia.com.co>
2022-08-04 18:21:52 -0500

    Update SophosApp.FibonacciServer - Using process counter like state (using Pattern Matching)

--- sophos_app/lib/sophos_app/fibonacci_server.ex
+++ sophos_app/lib/sophos_app/fibonacci_server.ex
@@ -2,18 +2,18 @@ defmodule SophosApp.FibonacciServer do
   alias SophosApp.Fibonacci
 
   def start() do
-    spawn(__MODULE__, :loop, [0])
+    spawn(__MODULE__, :loop, [{0}])
   end
 
   def start_link() do
-    spawn_link(__MODULE__, :loop, [0])
+    spawn_link(__MODULE__, :loop, [{0}])
   end
 
   def start_monitor() do
-    spawn_monitor(__MODULE__, :loop, [0])
+    spawn_monitor(__MODULE__, :loop, [{0}])
   end
 
-  def loop(counter) do
+  def loop({counter} = state) do
     receive do
       {:sequence, caller, n} ->
         result = Fibonacci.sequence(n)
@@ -21,15 +21,15 @@ defmodule SophosApp.FibonacciServer do
         loop(counter + 1)
 
       {:status, caller} ->
-        send(caller, {:ok, counter})
-        loop(counter)
+        send(caller, {:ok, state})
+        loop(state)
 
       {:exit, reason} ->
         IO.puts("bye for #{inspect(reason)}")
 
       _message ->
         IO.puts("Bad operation")
-        loop(counter)
+        loop(state)
       #after
       #  1500 -> IO.puts("Se acabó")
     end
