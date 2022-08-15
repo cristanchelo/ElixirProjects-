91215b08153fe6307015f47fa927b255f3a6c378
crbetan <crbetan@bancolombia.com.co>
2022-08-14 23:00:14 -0500

    Create SophosApp.PingPongProc module, it calls a handle_message with Ping Pong process in the same terminal. Delete example files from repository

--- sophos_app/lib/sophos_app/generic_server.ex
+++ sophos_app/lib/sophos_app/generic_server.ex
@@ -1,28 +1,48 @@
 defmodule SophosApp.GenericServer do
-  def start(module, init, caller_1 \\ self()) do
+  @moduledoc """
+  Módulo con las funciones generales para ejecución de procesos.
+  """
+
+  @doc """
+  Función para crear un proceso vinculado con estado.
+  """
+  def start(module, init, caller \\ self()) do
     Process.flag(:trap_exit, true)
-    spawn_link(__MODULE__, :loop, [module, caller_1, init])
+    spawn_link(__MODULE__, :loop, [module, caller, init])
   end
 
-  def start_processes(module, caller_1, caller_2) do
-    spawn( fn -> loop_processes(module, caller_1, caller_2, 0) end)
-  end
-
-  def loop(module, caller_1, state) do
+  @doc """
+  Función para llamar a la función handle_message al momento de recibir un mensaje de un proceso.
+  Obtiene el resultado y el estado del proceso.
+  """
+  def loop(module, caller, state) do
     receive do
       msg ->
-        {:ok, result, new_state} = module.handle_message(msg, caller_1, state)
-        send(caller_1, result)
-        loop(module, caller_1, new_state)
+        {:ok, result, new_state} = module.handle_message(msg, caller, state)
+        send(caller, result)
+        loop(module, caller, new_state)
     end
   end
 
-  def loop_processes(module, caller_1, caller_2, state) do
+  @doc """
+  Función para crear un proceso con estado.
+  Se relaciona con una función que recibe otro proceso y un mensaje.
+  """
+  def start_process(module, init, caller \\ self()) do
+    Process.flag(:trap_exit, true)
+    spawn(__MODULE__, :loop_change_process, [module, caller, init])
+  end
+
+  @doc """
+  Función para llamar a la función handle_message al momento de recibir un proceso y su mensaje.
+  Obtiene el resultado del proceso y hace un nuevo envio de mensaje con el proceso recibido por el anterior.
+  """
+  def loop_change_process(module, caller, state) do
     receive do
-      msg ->
-        {:ok, result, new_state} = module.handle_message(msg, caller_1, state)
-        send(caller_2, result)
-        loop_processes(module, caller_2, caller_1, new_state)
+      {pid_sender, msg} ->
+        {:ok, result, new_state} = module.handle_message({pid_sender, msg}, caller, state)
+        send(pid_sender, result)
+        loop_change_process(module, pid_sender, new_state)
     end
   end
 end
--- sophos_app/lib/sophos_app/ping_pong_proc.ex
+++ sophos_app/lib/sophos_app/ping_pong_proc.ex
@@ -1,19 +1,46 @@
 defmodule SophosApp.PingPongProc do
+  @moduledoc """
+  Módulo para ejecutar Ping Pong de procesos
 
-  def handle_message({:message, msg}, caller, state) do
-    IO.puts "PID #{inspect caller}, turn #{inspect msg}"
-    :timer.sleep(500)
-    {:ok, switch(msg), state + 1}
+  Basado en los siguientes ejemplos:
+  https://github.com/oelizondo/pingpong/blob/master/pingpong.exs
+  https://github.com/cristanchelo/ElixirProjects-/blob/master/sophos_app/lib/sophos_app/ping_pong_proc.ex
+  """
+  @doc """
+  Función para recibir el proceso a validar con su mensaje
+  """
+  def handle_message({pid_sender, turn}, caller, state) do
+    IO.puts "PID #{inspect pid_sender}, turn #{inspect turn}"
+    :timer.sleep(1000)
+    result = switch(turn)
+    {:ok, {self, result}, [result | state]}
   end
 
+  @doc """
+  Función para ejecutar estado de proceso
+  """
   def handle_message({:status}, caller, state ) do
     {:ok, state, state}
   end
 
+  @doc """
+  Función para cambiar turno: en caso de que la acción no sea :ping ni :pong, por defecto será :ping
+  """
   def switch(action) when action != :pong and action != :ping, do: :ping
+
+  @doc """
+  Función para cambiar turno en caso de que la acción sea :ping, cambiará el turno a :pong
+  """
   def switch(action) when action == :ping, do: :pong
+
+  @doc """
+  Función para cambiar turno en caso de que la acción sea :pong, cambiará el turno a :ping
+  """
   def switch(action) when action == :pong, do: :ping
 
+  @doc """
+  Función para crear procesos basados en el método switch. Usado solo de ejemplo.
+  """
   def start do
     ping_pid = spawn(__MODULE__, :switch, [:ping])
     pong_pid = spawn(__MODULE__, :switch, [:pong])
