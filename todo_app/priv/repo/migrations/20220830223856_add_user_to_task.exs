9c322a5df049cfed6a1a8e134feeb4855df639c1
crbetan <crbetan@bancolombia.com.co>
2022-08-31 22:50:58 -0500

    Update TodoApp.Todos.Task Schema and TodoApp.Repo.Migrations.AddUserToTask

--- todo_app/priv/repo/migrations/20220830223856_add_user_to_task.exs
+++ todo_app/priv/repo/migrations/20220830223856_add_user_to_task.exs
@@ -3,7 +3,7 @@ defmodule TodoApp.Repo.Migrations.AddUserToTask do
 
   def change do
     alter table(:tasks) do
-      add(:user_id, references(:users))
+      add(:user_id, references(:users), null: false)
     end
   end
 end
